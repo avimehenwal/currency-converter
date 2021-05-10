0983e35d8bddca955cd56df63e7b20ab3199d0b4
avimehenwal <avi.mehanwal@gmail.com>
2021-05-11 00:27:31 +0200

    feat(react/ts): dealing with form data
    1. formDAta web API method
    2. useRef
    3. onChange with useState hooks
    4. event.target.elements : React.FormEvent<HTMLFormElement>
    
    https://stackoverflow.com/questions/23427384/get-form-data-in-reactjs

--- frontend/src/currencyApp/component/Card/index.tsx
+++ frontend/src/currencyApp/component/Card/index.tsx
@@ -5,11 +5,19 @@ type defaultProps = {
 }
 
 export const Card: FC<defaultProps> = () => {
+
+  const formHandler = (event: React.FormEvent<HTMLFormElement>) => {
+    // @ts-ignore
+    const { source, amount, target } = event.target.elements
+    console.log(`source=${source.value} amount=${amount.value} target=${target.value}`);
+    event.preventDefault();
+  }
+
   return (
     <section className="card">
       <h2>source &rarr; target</h2>
 
-      <form>
+      <form name="converter" onSubmit={formHandler}>
         <label htmlFor="source">Source </label>
         <Select name="source" />
         <input name="amount" type="number"></input>

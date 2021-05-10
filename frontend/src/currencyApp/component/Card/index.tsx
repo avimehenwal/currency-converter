146339556e74028ef6414a4814db9200bcb78077
avimehenwal <avi.mehanwal@gmail.com>
2021-05-11 01:13:34 +0200

    feat(component): :bookmark: show results from backend

--- frontend/src/currencyApp/component/Card/index.tsx
+++ frontend/src/currencyApp/component/Card/index.tsx
@@ -1,21 +1,42 @@
-import React, { FC } from 'react'
+import React, { FC, useState } from 'react'
 import { Select } from '../Select'
+import { be, queryString } from '../../api'
+
 
 type defaultProps = {
 }
 
 export const Card: FC<defaultProps> = () => {
+  const [formData, setFormData] = useState<queryString>()
+  const [results, setResults] = useState()
 
   const formHandler = (event: React.FormEvent<HTMLFormElement>) => {
     // @ts-ignore
     const { source, amount, target } = event.target.elements
-    console.log(`source=${source.value} amount=${amount.value} target=${target.value}`);
+    const formData: queryString = {
+      base: source.value,
+      symbols: target.value,
+      amount: amount.value
+    }
+    // console.log(`source=${source.value} amount=${amount.value} target=${target.value}`);
+    setFormData(formData)
+
+    const apiResults = async () => {
+      const response = await be.convertCurrency(formData)
+      setResults(response.rates[formData.symbols])
+      console.dir(results)
+    }
+    apiResults();
     event.preventDefault();
   }
 
   return (
     <section className="card">
-      <h2>source &rarr; target</h2>
+
+      <div>
+        <h3>{formData?.amount} {formData?.base} &rarr; equals  </h3>
+        <h2>{results} {formData?.symbols}</h2>
+      </div><br />
 
       <form name="converter" onSubmit={formHandler}>
         <label htmlFor="source">Source </label>
@@ -24,10 +45,14 @@ export const Card: FC<defaultProps> = () => {
         <Select name="target" />
         <label htmlFor="target"> Target</label>
 
-        <br></br><br></br>
+        <br></br><br></br><br></br>
         <input type="submit" value="convert" />
+        <input type="reset" />
       </form>
 
+      <br></br>
+      <pre className="footer">{JSON.stringify(formData, null, 0)}</pre>
+      {/* <pre>{JSON.stringify(results, null, 0)}</pre> */}
     </section>
   )
 }
